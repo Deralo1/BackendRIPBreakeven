b93abec6437ac4e57f02cace0ada78310267c060
Deralo <javanet1321@gmail.com>
2025-10-06 20:40:15 +0300

    Добавить структуры нормальные для миграции доделать функции

--- internal/app/handler/handler.go
+++ internal/app/handler/handler.go
@@ -2,9 +2,6 @@ package handler
 
 import (
 	"Backeven/internal/app/repository"
-	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
@@ -20,31 +17,55 @@ func NewHandler(r *repository.Repository) *Handler {
 	}
 }
 
-func (h *Handler) GetAllServices(ctx *gin.Context) {
-	var services []repository.Service
-	var err error
+// RegisterHandler Функция в которой мы отдельно регистрируем маршруты,чтобы не писать все в одном месте
+func (h *Handler) RegisterHandler(router *gin.Engine) {
+	router.GET("/Nalogimain", h.GetAllExpense)
+	router.GET("/CostService/:id", h.GetExpenseByID)
+	//router.GET("/breakevencalc", h.GetBreakeven)
+}
 
-	searchQuery := ctx.Query("BreakenevSearch") // получаем значение из поля поиска
-	if searchQuery == "" {                      // если поле поиска пусто, то просто получаем из репозитория все записи
-		services, err = h.Repository.GetCostsService()
-		if err != nil {
-			logrus.Error(err)
-		}
-	} else {
-		services, err = h.Repository.GetServicesByTitle(searchQuery) // в ином случае ищем заказ по заголовку
-		if err != nil {
-			logrus.Error(err)
-		}
-	}
+// Получаем статику
+func (h *Handler) RegisterStatic(router *gin.Engine) {
+	router.LoadHTMLGlob("templates/*")
+	router.Static("/static", "./resources")
+}
 
-	ctx.HTML(http.StatusOK, "mainpageNalogi.html", gin.H{
-		"time":            time.Now().Format("15:04:05"),
-		"services":        services,
-		"BreakenevSearch": searchQuery, // передаем введенный запрос обратно на страницу
-		// в ином случае оно будет очищаться при нажатии на кнопку
+// errorhandler для удобного вывода ошибок
+func (h *Handler) errorhandler(ctx *gin.Context, errorStatusCode int, err error) {
+	logrus.Error(err.Error())
+	ctx.JSON(errorStatusCode, gin.H{
+		"status":      "error",
+		"description": err.Error(),
 	})
 }
-func (h *Handler) GetService(ctx *gin.Context) {
+
+/*
+	func (h *Handler) GetAllServices(ctx *gin.Context) {
+		var services []repository.Service
+		var err error
+
+		searchQuery := ctx.Query("BreakenevSearch") // получаем значение из поля поиска
+		if searchQuery == "" {                      // если поле поиска пусто, то просто получаем из репозитория все записи
+			services, err = h.Repository.GetCostsService()
+			if err != nil {
+				logrus.Error(err)
+			}
+		} else {
+			services, err = h.Repository.GetServicesByTitle(searchQuery) // в ином случае ищем заказ по заголовку
+			if err != nil {
+				logrus.Error(err)
+			}
+		}
+
+		ctx.HTML(http.StatusOK, "mainpageNalogi.html", gin.H{
+			"time":            time.Now().Format("15:04:05"),
+			"services":        services,
+			"BreakenevSearch": searchQuery, // передаем введенный запрос обратно на страницу
+			// в ином случае оно будет очищаться при нажатии на кнопку
+		})
+	}
+*/
+/*func (h *Handler) GetService(ctx *gin.Context) {
 	idStr := ctx.Param("id") // получаем id заказа из урла (то есть из /order/:id)
 	// через двоеточие мы указываем параметры, которые потом сможем считать через функцию выше
 	id, err := strconv.Atoi(idStr) // так как функция выше возвращает нам строку, нужно ее преобразовать в int
@@ -61,11 +82,11 @@ func (h *Handler) GetService(ctx *gin.Context) {
 		"service": service,
 	})
 }
-
-func (h *Handler) GetBreakeven(ctx *gin.Context) {
+*/
+/*func (h *Handler) GetBreakeven(ctx *gin.Context) {
 	CalcRequest := h.Repository.GetCalcServices()
 
 	ctx.HTML(http.StatusOK, "breakevenCalc.html", gin.H{
 		"CalcRequest": CalcRequest,
 	})
-}
+}*/
--- internal/app/repository/repository.go
+++ internal/app/repository/repository.go
@@ -1,11 +1,12 @@
 package repository
 
 import (
-	"fmt"
-	"strings"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Repository struct {
+	db *gorm.DB
 }
 
 func NewRepository() (*Repository, error) {
@@ -31,69 +32,84 @@ type ServiceForRequest struct {
 	TypeSpend     int
 }
 
-func (r *Repository) GetCostsService() ([]Service, error) {
-	// имитируем работу с БД. Типа мы выполнили sql запрос и получили эти строки из БД
-	services :=
-		[]Service{
-			{
-				ID:               1,
-				Title:            "Аренда склада",
-				ImageURL:         "http://localhost:9000/lab1/arenda.jpg",
-				Price:            1000000,
-				ShortDescription: "Средняя стоимость аренды склада в Москве",
-				Description:      "Аренда складских помещений востребована в Москве и области. Стоимость зависит от расположения: в пределах МКАД — 800–1200 ₽/м², в Подмосковье — 500–800 ₽/м². В логистических парках цена ниже, но склады находятся дальше от центра. При выборе учитывают транспортную доступность, наличие охраны и инфраструктуры.",
-			},
-			{
-				ID:               2,
-				Title:            "Оклад",
-				ImageURL:         "http://localhost:9000/lab1/oklad.jpg",
-				Price:            168000,
-				ShortDescription: "Средний оклад по отрасли",
-				Description:      "Оклад — фиксированная часть заработной платы, которая выплачивается сотруднику независимо от объёма выполненной работы. В среднем по отрасли он составляет около 168 000 ₽. Конкретная сумма зависит от должности, региона и уровня квалификации.",
-			},
-			{
-				ID:               3,
-				Title:            "Сырьё",
-				ImageURL:         "http://localhost:9000/lab1/siriy.jpg",
-				Price:            500,
-				ShortDescription: "Стоимость сырья для производства 1кг продукции",
-				Description:      "Сырьё — это материалы, используемые для производства продукции. Цена зависит от вида: металл, древесина, пластик, текстиль и т.д. На стоимость влияют мировые котировки, логистика и объём закупки.",
-			},
-			{
-				ID:               4,
-				Title:            "Сдельная зарплата",
-				ImageURL:         "http://localhost:9000/lab1/sdelnay.jpg",
-				Price:            10,
-				ShortDescription: "Оплата за единицу продукции",
-				Description:      "Сдельная форма оплаты труда предполагает, что работник получает деньги за каждую произведённую единицу продукции или выполненную операцию. Такой подход стимулирует производительность, но требует строгого контроля качества.",
-			},
-			{
-				ID:               5,
-				Title:            "Аренда торгового помещения",
-				ImageURL:         "http://localhost:9000/lab1/arendratorgpom.png",
-				Price:            10000,
-				ShortDescription: "Аренда торговых площадей в Москве",
-				Description:      "Аренда торговых помещений востребована в крупных городах. Цена зависит от расположения: в центре Москвы — от 10 000 ₽/м², в спальных районах — дешевле. Важны проходимость, наличие парковки и транспортная доступность.",
-			},
-			{
-				ID:               6,
-				Title:            "Транспортировка",
-				ImageURL:         "http://localhost:9000/lab1/transportation.jpg",
-				Price:            20,
-				ShortDescription: "Стоимость за киллометр транспортировки",
-				Description:      "Транспортировка включает доставку товаров автомобильным, железнодорожным или морским транспортом. Средняя цена — 20 ₽ за километр. На стоимость влияют расстояние, вес груза и выбранный вид транспорта.",
-			},
-		}
-
-	// обязательно проверяем ошибки, и если они появились - передаем выше, то есть хендлеру
-	// тут я снова искусственно обработаю "ошибку" чисто чтобы показать вам как их передавать выше
-	if len(services) == 0 {
-		return nil, fmt.Errorf("массив пустой")
+func New(dsn string) (*Repository, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{}) // подключаемся к БД
+	if err != nil {
+		return nil, err
 	}
 
-	return services, nil
+	// Возвращаем объект Repository с подключенной базой данных
+	return &Repository{
+		db: db,
+	}, nil
 }
-func (r *Repository) GetService(id int) (Service, error) {
+
+/*
+	func (r *Repository) GetCostsService() ([]Service, error) {
+		// имитируем работу с БД. Типа мы выполнили sql запрос и получили эти строки из БД
+		services :=
+			[]Service{
+				{
+					ID:               1,
+					Title:            "Аренда склада",
+					ImageURL:         "http://localhost:9000/lab1/arenda.jpg",
+					Price:            1000000,
+					ShortDescription: "Средняя стоимость аренды склада в Москве",
+					Description:      "Аренда складских помещений востребована в Москве и области. Стоимость зависит от расположения: в пределах МКАД — 800–1200 ₽/м², в Подмосковье — 500–800 ₽/м². В логистических парках цена ниже, но склады находятся дальше от центра. При выборе учитывают транспортную доступность, наличие охраны и инфраструктуры.",
+				},
+				{
+					ID:               2,
+					Title:            "Оклад",
+					ImageURL:         "http://localhost:9000/lab1/oklad.jpg",
+					Price:            168000,
+					ShortDescription: "Средний оклад по отрасли",
+					Description:      "Оклад — фиксированная часть заработной платы, которая выплачивается сотруднику независимо от объёма выполненной работы. В среднем по отрасли он составляет около 168 000 ₽. Конкретная сумма зависит от должности, региона и уровня квалификации.",
+				},
+				{
+					ID:               3,
+					Title:            "Сырьё",
+					ImageURL:         "http://localhost:9000/lab1/siriy.jpg",
+					Price:            500,
+					ShortDescription: "Стоимость сырья для производства 1кг продукции",
+					Description:      "Сырьё — это материалы, используемые для производства продукции. Цена зависит от вида: металл, древесина, пластик, текстиль и т.д. На стоимость влияют мировые котировки, логистика и объём закупки.",
+				},
+				{
+					ID:               4,
+					Title:            "Сдельная зарплата",
+					ImageURL:          "Аренда торгового помещения",
+					ImageURL:         "http://localhost:9000/lab1/sdelnay.jpg",
+					Price:            10,
+					ShortDescription: "Оплата за единицу продукции",
+					Description:      "Сдельная форма оплаты труда предполагает, что работник получает деньги за каждую произведённую единицу продукции или выполненную операцию. Такой подход стимулирует производительность, но требует строгого контроля качества.",
+				},
+				{
+					ID:               5,
+					Title:            "Аренда торгового помещения",
+					ImageURL:         "http://localhost:9000/lab1/arendratorgpom.png",
+					Price:            10000,
+					ShortDescription: "Аренда торговых площадей в Москве",
+					Description:      "Аренда торговых помещений востребована в крупных городах. Цена зависит от расположения: в центре Москвы — от 10 000 ₽/м², в спальных районах — дешевле. Важны проходимость, наличие парковки и транспортная доступность.",
+				},
+				{
+					ID:               6,
+					Title:            "Транспортировка",
+					ImageURL:         "http://localhost:9000/lab1/transportation.jpg",
+					Price:            20,
+					ShortDescription: "Стоимость за киллометр транспортировки",
+					Description:      "Транспортировка включает доставку товаров автомобильным, железнодорожным или морским транспортом. Средняя цена — 20 ₽ за километр. На стоимость влияют расстояние, вес груза и выбранный вид транспорта.",
+				},
+			}
+
+		// обязательно проверяем ошибки, и если они появились - передаем выше, то есть хендлеру
+		// тут я снова искусственно обработаю "ошибку" чисто чтобы показать вам как их передавать выше
+		if len(services) == 0 {
+			return nil, fmt.Errorf("массив пустой")
+		}
+
+		return services, nil
+	}
+*/
+/*func (r *Repository) GetService(id int) (Service, error) {
 	// тут у вас будет логика получения нужной услуги, тоже наверное через цикл в первой лабе, и через запрос к БД начиная со второй
 	services, err := r.GetCostsService()
 	if err != nil {
@@ -106,8 +122,8 @@ func (r *Repository) GetService(id int) (Service, error) {
 		}
 	}
 	return Service{}, fmt.Errorf("заказ не найден") // тут нужна кастомная ошибка, чтобы понимать на каком этапе возникла ошибка и что произошло
-}
-func (r *Repository) GetServicesByTitle(title string) ([]Service, error) {
+}*/
+/*func (r *Repository) GetServicesByTitle(title string) ([]Service, error) {
 	services, err := r.GetCostsService()
 	if err != nil {
 		return []Service{}, err
@@ -121,10 +137,10 @@ func (r *Repository) GetServicesByTitle(title string) ([]Service, error) {
 	}
 
 	return result, nil
-}
-func (r *Repository) GetCalcServices() *BreakevenRequest {
-	card1, _ := r.GetService(1)
-	card2, _ := r.GetService(2)
+}*/
+/*func (r *Repository) GetCalcServices() *BreakevenRequest {
+	card1, _ := r.GetExpenseByID(1)
+	card2, _ := r.GetExpenseByID(2)
 
 	CurrentCalc := &BreakevenRequest{
 		AmountProduct: 120,
@@ -135,4 +151,4 @@ func (r *Repository) GetCalcServices() *BreakevenRequest {
 		},
 	}
 	return CurrentCalc
-}
+}*/
